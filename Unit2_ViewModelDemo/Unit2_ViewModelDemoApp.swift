1cdeb183a5d8f889cdd7db30579d9d12a636da7e
Madeline Burton <mburton40@gatech.edu>
2023-11-08 12:51:14 -0600

    Initial Commit

---
+++ Unit2_ViewModelDemo/Unit2_ViewModelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit2_ViewModelDemoApp.swift
+//  Unit2_ViewModelDemo
+//
+//  Created by Madeline Burton on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit2_ViewModelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
